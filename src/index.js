d3c97e330ddb2bb33a14549620d3aa3774b3f847
yuliyasamsonova <yuliyasamsonova01@gmail.com>
2019-02-22 00:20:47 +0300

    loveTriangleCounter

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = function getLoveTrianglesCount(a = []) {
+  var counter =0;
+  for (k=0;k< a.length; k++){
+      if (a[a[a[k]-1]-1]== k+1){
+          counter++;
+      }
+  }
+  return (parseInt(counter/3));
 };
